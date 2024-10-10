968b05c52b2da87869fecff8846f8e18db8c4873
trungdung1711 <trungdunglebui17112004@gmail.com>
2024-10-10 17:44:51 +0700

    dung-commenting the security package in pom.xml, adding [banner.txt], changing format in main application

--- back-end/src/main/java/com/dnth_underdog_241/online_fashion_shopping/OnlineFashionShoppingApplication.java
+++ back-end/src/main/java/com/dnth_underdog_241/online_fashion_shopping/OnlineFashionShoppingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlineFashionShoppingApplication {
-
-	public static void main(String[] args) {
+public class OnlineFashionShoppingApplication 
+{
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(OnlineFashionShoppingApplication.class, args);
 	}
-
-}
+};
