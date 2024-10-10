d0820decea4b1012c6ae6e6a13131daa2a3e6ad2
trungdung1711 <trungdunglebui17112004@gmail.com>
2024-10-10 17:31:55 +0700

    dung-creating the basic directory for the back-end

---
+++ back-end/src/main/java/com/dnth_underdog_241/online_fashion_shopping/OnlineFashionShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.dnth_underdog_241.online_fashion_shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFashionShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFashionShoppingApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/dnth_underdog_241/online_fashion_shopping/OnlineFashionShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnth_underdog_241.online_fashion_shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFashionShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
